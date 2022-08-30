dde91954651e4ea5708d67f21cbd0a78014efe2c
Dimitrije <dimitrije.zarkovic@material-exchange.com>
2022-08-30 22:08:13 +0200

    feat: consumer interface added

---
+++ BusService/BusService/IConsumer.cs
@@ -0,0 +1,10 @@
+﻿using NATS.Client;
+using Polly;
+
+namespace BusService
+{
+    public interface IConsumer
+    {
+        EventHandler<MsgHandlerEventArgs> BuildConsumerMethod(Policy policy);
+    }
+}

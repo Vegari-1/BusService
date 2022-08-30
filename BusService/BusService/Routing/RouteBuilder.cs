d2d0013a26c84a0544f15bae8cbf08d80593e364
Dimitrije <dimitrije.zarkovic@material-exchange.com>
2022-08-30 21:35:49 +0200

    feat: GetEventName method added to RouteBuilder

--- BusService/BusService/Routing/RouteBuilder.cs
+++ BusService/BusService/Routing/RouteBuilder.cs
@@ -9,5 +9,6 @@ namespace BusService.Routing
         private static readonly string WILDCARD_ANY_TAIL = ">";
 
         public static string Build(string Topic, [Optional] string Event) => Topic + SEPARATOR + (Event ?? WILDCARD_ANY);
+        public static string GetEventName(string Source) => Source.Split(SEPARATOR).Last();
     }
 }

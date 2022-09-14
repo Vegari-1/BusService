d2030318330198e256ab6ced6a9a36d649fe8de3
Eva Janković <evaj.evadesign@gmail.com>
2022-09-14 12:07:57 +0200

    feat: additional contracts and topics

---
+++ BusService/BusService/Contracts/ConnectionContract.cs
@@ -0,0 +1,17 @@
+﻿namespace BusService.Contracts
+{
+	public class ConnectionContract
+    {
+        public Guid Id { get; set; }
+        public Guid Profile1 { get; set; }
+        public Guid Profile2 { get; set; }
+
+        public ConnectionContract(Guid id, Guid profile1, Guid profile2)
+        {
+            Id = id;
+            Profile1 = profile1;
+            Profile2 = profile2;
+        }
+    }
+}
+
---
+++ BusService/BusService/Contracts/EventContract.cs
@@ -0,0 +1,23 @@
+﻿namespace BusService.Contracts
+{
+	public class EventContract
+    {
+        public DateTime Timestamp { get; set; }
+        public string Source { get; set; }
+        public string RequestType { get; set; }
+        public string Message { get; set; }
+        public int StatusCode { get; set; }
+        public string StatusCodeText { get; set; }
+
+        public EventContract(DateTime timestamp, string source, string requestType, string message, int statusCode, string statusCodeText)
+        {
+            Timestamp = timestamp;
+            Source = source;
+            RequestType = requestType;
+            Message = message;
+            StatusCode = statusCode;
+            StatusCodeText = statusCodeText;
+        }
+    }
+}
+

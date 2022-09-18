56c0ed3ebc38abca2d98277f4a800f146b47142e
Eva Janković <evaj.evadesign@gmail.com>
2022-09-18 14:33:24 +0200

    feat: add post and message contract

---
+++ BusService/BusService/Contracts/MessageContract.cs
@@ -0,0 +1,15 @@
+﻿namespace BusService.Contracts
+{
+	public class MessageContract
+    {
+        public Guid SenderId { get; set; }
+        public Guid ReceiverId { get; set; }
+
+        public MessageContract(Guid senderId, Guid receiverId)
+        {
+            SenderId = senderId;
+            ReceiverId = receiverId;
+        }
+    }
+}
+
